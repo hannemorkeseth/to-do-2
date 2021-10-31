e9ed315fbf50711235cf64e5daf6545bcc8ac0a3
hannemorkeseth <91186467+hannemorkeseth@users.noreply.github.com>
2021-10-31 17:23:45 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+import todoData from './todoData'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todoData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const newTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+      return {
+        todos: newTodos
+      }
+    })
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
+    return (
+      <div>
+        {todoItems}
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/todoData.js
@@ -0,0 +1,19 @@
+const todoData = [
+    {
+        id: 1,
+        text: "Aktivitet 1",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Aktivitet 2",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Aktivitet 3",
+        completed: false
+    }
+]
+
+export default todoData
